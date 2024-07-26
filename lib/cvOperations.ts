c1f03297088f2545009c0c7406dec0f79d50d567
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-07-27 02:56:03 +0930

    (updated) getting the CV

--- lib/cvOperations.ts
+++ lib/cvOperations.ts
@@ -10,3 +10,14 @@ export async function createCV(cv: CV) {
   if (error) throw error
   return data
 }
+
+export async function getCV(id: string) {
+	const { data, error } = await supabase
+	  .from('cvs')
+	  .select('*')
+	  .eq('id', id)
+	  .single()
+  
+	if (error) throw error
+	return data
+  }
